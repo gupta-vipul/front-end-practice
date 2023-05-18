001f152fdc5a39881d61b1be551c2adc8b12ebe3
Vipul Gupta <mail.vipulg@gmail.com>
2023-05-18 10:32:03 +0530

    Start Learning JS
    I learned about JS variables
    
    lil bit high level idea about network, source, & console option in chrome developer tools

---
+++ jstutorial001.js
@@ -0,0 +1,15 @@
+var firstName = "Vipul";
+
+console.log(firstName);
+
+console.log(typeof(firstName));
+
+var a = "We are learning JavaScipt";
+console.log("Line no: 8","type of variable["+ typeof a+"]", a);
+
+a = 1234;
+console.log("Line no: 10", typeof a, a);
+
+var a = 456;
+console.log("Line no: 13", typeof a, a);
+
