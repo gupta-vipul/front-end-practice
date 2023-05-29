d1eaaf2103ec490cd4af30003ff44f059431464c
Vipul Gupta <mail.vipulg@gmail.com>
2023-05-29 23:28:35 +0530

    Instructor 2 Practice project using grid

--- Learning JS/jstutorial002.js
+++ Learning JS/jstutorial002.js
@@ -1,2 +1,15 @@
 // console.log("Testing from 2 source file");
 
+var concatenate = 20+30+"30"; // First compiler 
+console.log(concatenate);  
+
+var concatenate2 = "10"+20+30;
+console.log(concatenate2);
+
+
+var tt = +("20" + "30" + "40") + +"120" + "230";
+console.log(tt);
+console.log(typeof tt);
+
+var tts = -('20'+'30'+'50')-('30'-'30'+60);
+console.log(tts);
